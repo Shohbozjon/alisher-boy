5f71f183b306f0c8c4cf5a9bf0dd585e28a24611
Shohbozjon <shohbozjonquvondiqov@gmail.com>
2022-02-21 20:51:41 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var a = 500;
+var b = 250;
+var c = 120;
+var amount = Number(prompt("Sayohat uchun kerakli pul miqdori:" , 0));
+
+// so'mga o'girish:
+A = a * 10900;
+B = b * 10900;
+C = c * 12272.64;
+
+var S = Number(A+B+C);
+
+if (amount >= S){
+  console.log("Oq yo'l, Alisher!");
+}else{
+  console.log("Alisher, ozgina sabr qilish bo'lar ekan.");
+}
+
+
+
+
+
+
+
